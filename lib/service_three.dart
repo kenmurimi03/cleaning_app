8fa8d7f6d84d42ecafc280c3e966a452df498f59
kenmurimi03 <murimikennedy69@gmail.com>
2025-05-14 20:54:08 +0300

    Introduce 'register user' method

--- lib/service_three.dart
+++ lib/service_three.dart
@@ -1,8 +1,11 @@
 import 'package:cleansafi/color_pallette.dart';
 import 'package:cleansafi/one_time.dart';
+import 'package:cleansafi/outdoors_review.dart';
 import 'package:cleansafi/recurrent.dart';
+import 'package:cleansafi/services_offered.dart';
+import 'package:cleansafi/you_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/svg.dart';
 
 class ServicesthreePage extends StatefulWidget {
   const ServicesthreePage({
@@ -14,17 +17,71 @@ class ServicesthreePage extends StatefulWidget {
 }
 
 class _ServicesthreePageState extends State<ServicesthreePage> {
-  final ValueNotifier<String> selectedBedroomNotifier =
+  final ValueNotifier<String> selectedCompoundSizeNotifier =
       ValueNotifier<String>('Select compound size');
 
   final currentIndexNotifier = ValueNotifier<int>(0);
+  final _locationController = TextEditingController();
+  final _houseController = TextEditingController();
+  final _phoneNumberController = TextEditingController();
+  final _customerNameController = TextEditingController();
+
+  void _registerUser() async {
+    String location = _locationController.text.trim();
+    String house = _houseController.text.trim();
+    String phoneNumber = _phoneNumberController.text.trim();
+    String customerName = _customerNameController.text.trim();
+
+    if (location.isEmpty ||
+        house.isEmpty ||
+        phoneNumber.isEmpty ||
+        customerName.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Please fill in all fields."),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    } else {
+      if (mounted) {
+        showDialog<void>(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              elevation: 2,
+              backgroundColor: AppTheme.kBackgroundColor,
+              child: UserrDetailsScreen(
+                location: location,
+                house: house,
+                phoneNumber: phoneNumber,
+                customerName: customerName,
+                landSize: selectedCompoundSizeNotifier.value,
+                
+              ),
+            );
+          },
+        );
+      }
+    }
+  }
 
   @override
   void initState() {
     super.initState();
   }
 
-  void filterForm() {}
+  @override
+  void dispose() {
+    _locationController.dispose();
+    _houseController.dispose();
+    _phoneNumberController.dispose();
+    _customerNameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -106,6 +163,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                     child: Padding(
                       padding: const EdgeInsets.only(left: 12, right: 12),
                       child: TextFormField(
+                        controller: _locationController,
                         style: const TextStyle(
                           fontFamily: 'Helvetica Neue',
                           color: AppTheme.kBlackColor,
@@ -178,6 +236,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                     child: Padding(
                       padding: const EdgeInsets.only(left: 12, right: 12),
                       child: TextFormField(
+                        controller: _houseController,
                         style: const TextStyle(
                           fontFamily: 'Helvetica Neue',
                           color: AppTheme.kBlackColor,
@@ -231,7 +290,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                     child: Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
-                        'Who should our cleaner contact?',
+                        'The phone number our cleaner is to contact',
                         style: TextStyle(
                           fontSize: 14,
                           fontStyle: FontStyle.normal,
@@ -250,6 +309,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                     child: Padding(
                       padding: const EdgeInsets.only(left: 12, right: 12),
                       child: TextFormField(
+                        controller: _phoneNumberController,
                         style: const TextStyle(
                           fontFamily: 'Helvetica Neue',
                           color: AppTheme.kBlackColor,
@@ -319,6 +379,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                     child: Padding(
                       padding: const EdgeInsets.only(left: 12, right: 12),
                       child: TextFormField(
+                        controller: _customerNameController,
                         style: const TextStyle(
                           fontFamily: 'Helvetica Neue',
                           color: AppTheme.kBlackColor,
@@ -425,7 +486,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                         child: Align(
                           alignment: Alignment.centerLeft,
                           child: ValueListenableBuilder<String>(
-                            valueListenable: selectedBedroomNotifier,
+                            valueListenable: selectedCompoundSizeNotifier,
                             builder: (
                               BuildContext context,
                               String selectedValue,
@@ -433,7 +494,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                             ) {
                               return PopupMenuButton<String>(
                                 onSelected: (newValue) {
-                                  selectedBedroomNotifier.value = newValue;
+                                  selectedCompoundSizeNotifier.value = newValue;
                                 },
                                 surfaceTintColor: AppTheme.kBackgroundColor,
                                 tooltip: 'Choose section',
@@ -448,7 +509,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                                 ),
                                 itemBuilder: (context) => [
                                   const PopupMenuItem(
-                                    value: 'eight of an acre',
+                                    value: 'eighth of an acre',
                                     height: 20,
                                     child: Text(
                                       'eight of an acre',
@@ -512,7 +573,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                                             MainAxisAlignment.spaceBetween,
                                         children: [
                                           Text(
-                                            selectedBedroomNotifier.value,
+                                            selectedCompoundSizeNotifier.value,
                                             style: const TextStyle(
                                               fontSize: 14,
                                               fontStyle: FontStyle.normal,
@@ -566,7 +627,7 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                       ),
                     ),
                   ),
-                 const SizedBox(
+                  const SizedBox(
                     height: 5,
                   ),
                   OneTime(),
@@ -577,8 +638,121 @@ class _ServicesthreePageState extends State<ServicesthreePage> {
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: MaterialButton(
+                color: AppTheme.pickerColor3,
+                minWidth: double.infinity,
+                height: 36,
+                elevation: 3,
+                onPressed: _registerUser,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: const Text(
+                  'Save details and review specifics',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    color: AppTheme.kBackgroundColor,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Helvetica Neue',
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+      bottomNavigationBar: ValueListenableBuilder(
+        valueListenable: currentIndexNotifier,
+        builder: (context, currentIndex, _) {
+          return BottomNavigationBar(
+            selectedIconTheme: const IconThemeData(
+              color: AppTheme.kBlackColor,
+            ),
+            selectedItemColor: Colors.black,
+            backgroundColor: AppTheme.kBackgroundColor,
+            currentIndex: currentIndex,
+            unselectedLabelStyle: const TextStyle(
+              color: Colors.black,
+              fontFamily: 'Helvetica Neue',
+              fontSize: 11,
+              fontWeight: FontWeight.w700,
+            ),
+            selectedLabelStyle: const TextStyle(
+              color: Colors.black,
+              fontFamily: 'Helvetica Neue',
+              fontSize: 11,
+              fontWeight: FontWeight.w700,
+            ),
+            onTap: (int index) {
+              currentIndexNotifier.value = index;
+            },
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: SizedBox(
+                      height: 25,
+                      width: 25,
+                      child: SvgPicture.asset(
+                        currentIndex == 0
+                            ? 'assets/active_home_tab_icon.svg'
+                            : 'assets/inactive_home_tab_icon.svg',
+                      ),
+                    ),
+                  ),
+                ),
+                label: 'home',
+              ),
+              const BottomNavigationBarItem(
+                icon: Padding(
+                  padding: EdgeInsets.only(bottom: 4),
+                  child: SizedBox.shrink(),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(bottom: 4),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => YouPage()),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: SizedBox(
+                      height: 25,
+                      width: 25,
+                      child: SvgPicture.asset(
+                        currentIndex == 3
+                            ? 'assets/active_you_tab_icon.svg'
+                            : 'assets/inactive_you_tab_icon.svg',
+                      ),
+                    ),
+                  ),
+                ),
+                label: 'you',
+              ),
+            ],
+          );
+        },
       ),
     );
   }
